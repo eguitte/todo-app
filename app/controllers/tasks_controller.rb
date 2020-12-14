0c71a1cc30b2ae997543fe1456f36a8002e19682
naoki.kubota <nk.0110.6@gmail.com>
2020-12-14 11:04:33 +0000

    resubmit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,11 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy, :update, :show]
+  before_action :correct_user, only: [:destroy, :update, :show, :edit]
   
+ 
   def index
-    if logged_in?
       @task = current_user.tasks.build
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-    end
   end
 
   def show
